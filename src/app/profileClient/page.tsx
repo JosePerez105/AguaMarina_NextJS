5cfb8a820aee471892a83c63d7006a4e93c39a85
Emmanuelvg8 <emmanuelvg506@gmail.com>
2024-11-05 20:19:32 -0500

    Modificacion con agenda

--- src/app/profileClient/page.tsx
+++ src/app/profileClient/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
       <ClienteLayout>
         <>
-          <Breadcrumb pageName="Contacto" />
+          <Breadcrumb pageName="Perfil" />
           <ProfileClient />
         </>
       </ClienteLayout>
